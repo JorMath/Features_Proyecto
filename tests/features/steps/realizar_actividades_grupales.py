857aaa7463cc63e880f350712549be9356370f83
Jorman Chuquer <jorman.chuquer@epn.edu.ec>
2024-12-18 00:25:22 -0500

    creación de steps con behave

---
+++ tests/features/steps/realizar_actividades_grupales.py
@@ -0,0 +1,67 @@
+from behave import *
+
+
+
+
+@step("que existen espacios publicos disponibles para reservar")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    raise NotImplementedError(u'STEP: Dado que existen espacios publicos disponibles para reservar')
+
+
+@step("el aforo maximo de este espacio publico es 30 personas")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    raise NotImplementedError(u'STEP: Y el aforo maximo de este espacio publico es 30 personas')
+
+
+@step("existe una franja de tiempo disponible para esa actividad")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    raise NotImplementedError(u'STEP: Y existe una franja de tiempo disponible para esa actividad')
+
+
+@step("un ciudadano inicie una actividad en el espacio publico")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    raise NotImplementedError(u'STEP: Cuando un ciudadano inicie una actividad en el espacio publico')
+
+
+@step("el espacio publico será ocupado para realizar esa actividad")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    raise NotImplementedError(u'STEP: Entonces el espacio publico será ocupado para realizar esa actividad')
+
+
+@step("se publicará la actividad en la agenda de actividades públicas")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    raise NotImplementedError(u'STEP: Y se publicará la actividad en la agenda de actividades públicas')
+
+
+@step("se tiene una lista de asistentes a la actividad")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    raise NotImplementedError(u'STEP: Y se tiene una lista de asistentes a la actividad')
+
+
+@step("se notificará la actividad a los asistentes de la lista")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    raise NotImplementedError(u'STEP: Entonces se notificará la actividad a los asistentes de la lista')
